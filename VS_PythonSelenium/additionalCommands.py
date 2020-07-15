1667bcee64dd6f8135bbe564abe0d34700b554b2
raykhon0204 <55024131+raykhon0204@users.noreply.github.com>
2020-07-14 21:09:25 -0400

    changed

--- VS_PythonSelenium/additionalCommands.py
+++ VS_PythonSelenium/additionalCommands.py
@@ -1,4 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import time
 driver = webdriver.Chrome(executable_path='C:\\Program Files (x86)\\Drivers\\chromedriver.exe')
+driver.get("http://gmail.com/")
+elem1=driver.find_element_by_xpath("/html/body")
+time.sleep(5)
+email = 'raykhon.juraeva@gmail.com'
+elem1.send_keys(email)
+
+next_btn = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]').click()
+
+
+
